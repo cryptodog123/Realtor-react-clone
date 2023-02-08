be89bd1f62a8e87803b7723d87d4a3ed7f87c20a
cryptodog123 <stornytmo@gmail.com>
2023-02-08 19:26:35 +0100

    Completing sign-in functionality

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { FcGoogle } from "react-icons/fc";
+
+import { toast } from "react-toastify";
 import { getAuth } from "firebase/auth";
 import { GoogleAuthProvider } from "firebase/auth";
-import { toast } from "react-toastify";
+
 import { signInWithPopup } from "firebase/auth";
 import { db } from "../firebase";
 import { getDoc, doc, setDoc, serverTimestamp } from "firebase/firestore";
@@ -10,12 +12,13 @@ import { useNavigate } from "react-router-dom";
 
 const OAuth = () => {
   const navigate = useNavigate();
-  const googleSignUp = async () => {
+
+  const oAuthCallback = async () => {
     try {
       const auth = getAuth();
       const provider = new GoogleAuthProvider();
       const response = await signInWithPopup(auth, provider);
-      console.log(response.user);
+
       const docRef = doc(db, "users", response.user.uid);
       const docSnap = await getDoc(docRef);
       if (!docSnap.exists()) {
@@ -34,7 +37,7 @@ const OAuth = () => {
 
   return (
     <button
-      onClick={googleSignUp}
+      onClick={oAuthCallback}
       type="button"
       className="w-full py-2 hover:bg-red-600 transition ease-in bg-red-500 text-white shadow-md hover:shadow-lg active:bg-red-700 flex gap-1 items-center justify-center"
     >
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -5,18 +5,40 @@ import Input from "../components/Input";
 import PasswordInput from "../components/SignInSignUp/PasswordInput";
 import SmallText from "../components/SignInSignUp/SmallText";
 import Buttons from "../components/SignInSignUp/Buttons";
-
+import { signInWithEmailAndPassword, getAuth } from "firebase/auth";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 const SignIn = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
+  const navigate = useNavigate();
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const auth = getAuth();
+      const userCredentials = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      // User exists
+      if (userCredentials.user) {
+        navigate("/");
+        toast.success("Successfully logged inn.");
+      }
+    } catch (err) {
+      toast.error("Bad user credentials");
+    }
+  };
 
   return (
     <section>
       <div className=" relative flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto pt-[125px]">
         <HeaderLeft title={"Sign In"} />
         <div className="sm:w-full md:w-[67%] lg:w-[40%] lg:ml-20">
-          <form>
+          <form onSubmit={onSubmit}>
             <Input
               defaultValue={email}
               placeholder={"Email@example.com"}
